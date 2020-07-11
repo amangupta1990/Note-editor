dc335d01c5cb4bba8ce4f65c066231f81f5b5aaf
Aman Gupta <amangupta.1990@ymail.com>
2020-07-11 17:33:20 +0530

    add custom Id for selections

--- src/components/SheetEditor/editor.js/index.js
+++ src/components/SheetEditor/editor.js/index.js
@@ -6,6 +6,9 @@ const REST_POSITIONS = {
   "w": "e/5",
 }
 
+
+
+  
 class Editor {
 
   //eslint-disable-next-line
@@ -26,6 +29,7 @@ class Editor {
     this.noteWidth = 40;
     this.dotted = "";
     this.formatter = Vex.Flow.Formatter,
+    this.svgElem = svgcontainer;
     this.renderer = new Vex.Flow.Renderer(
       svgcontainer,
       Vex.Flow.Renderer.Backends.SVG
@@ -46,9 +50,9 @@ class Editor {
 
     // add first stave by default
     this.addStave()
-    this.addNote(this.sheet.staves[0],0,"c/4","q")
-    this.addNote(this.sheet.staves[0],1,"c/4","q")
-    this.deleteNote(this.sheet.staves[0],0)
+    this.addNote(0,0,"c/4","q")
+    this.addNote(0,1,"c/4","q")
+    this.deleteNote(0,0)
     // this.addNote(this.sheet.staves[0],"c/4","q")
     //this.editNote(this.sheet.staves[0],1,"c/4","w")
   }
@@ -58,13 +62,16 @@ class Editor {
   addStave(index = this.sheet.staves ? this.sheet.staves.length : 0 ) {
     this.sheet.staves = this.sheet.staves ||  [];
     let stave = new Vex.Flow.Stave(10,40, this.staveWidth*(this.sheet.staves.length+1));
-    stave.setAttribute("measureID",index)
+    stave.setAttribute("id",index)
     stave.addClef(this.clef).addTimeSignature("4/4");
     stave.notes = new Array(4).fill("q");
 
     // fill it up with rests 
-    stave.notes =  stave.notes.map((d)=> { 
-      return  new Vex.Flow.StaveNote({clef: this.clef, keys: [REST_POSITIONS[d]], duration:d+"r"})
+    stave.notes =  stave.notes.map((d,noteIndex)=> { 
+      let n =   new Vex.Flow.StaveNote({clef: this.clef, keys: [REST_POSITIONS[d]], duration:d+"r"})
+      n.setAttribute("id",`${index}|${noteIndex}`);
+
+      return n;
      } )
     
     var voice = new Vex.Flow.Voice({num_beats: 4,  beat_value: 4});
@@ -79,30 +86,25 @@ class Editor {
     
   }
 
-  addNote(stave,index,note,duration){
+  addNote(staveIndex,noteIndex,noteName,duration){
     // modify the rest of the stave to join the notes 
+    let stave = this.sheet.staves[staveIndex]
     let notes = stave.notes;
-    let n  = new Vex.Flow.StaveNote({clef: this.clef, keys: [note], duration });
-    n.setAttribute("noteID",index);
-    n.setAttribute("MeasureID",index);
-    notes[index] = n;
+    let n  = new Vex.Flow.StaveNote({clef: this.clef, keys: [noteName], duration });
+    n.setAttribute("id",`${staveIndex}|${noteIndex}`);
+
+    notes[noteIndex] = n;
     this.reDraw()
     
 
   }
-  editNote(stave,noteIndex,note,newDuration){
-    let newNote  = new Vex.Flow.StaveNote({clef: this.clef, keys: [note], duration: newDuration });
-    stave.notes[noteIndex] = newNote;
-    this.reDraw();
-
-
-  }
-
-  deleteNote(stave,index){
+  deleteNote(staveIndex,noteIndex){
     // convert the note into a rest 
-    let oldNote =  stave.notes[index];
+    let stave = this.sheet.staves[staveIndex]
+    let oldNote =  stave.notes[noteIndex];
     let n = new Vex.Flow.StaveNote({clef: this.clef, keys: [REST_POSITIONS[oldNote.duration]], duration: oldNote.duration+'r' });
-    stave.notes[index]= n;
+    n.setAttribute("id",oldNote.id)
+    stave.notes[noteIndex]= n;
     this.reDraw()
   }
 
@@ -116,7 +118,77 @@ class Editor {
       new Vex.Flow.Formatter().format([stave.voice], this.staveWidth);
       stave.voice.draw(this.ctx,stave);
     })
-  }  
+  }
+  
+  
+  //  getMousePos(canvas, evt) {
+  //   var rect = canvas.getBoundingClientRect();
+  //     return {
+  //       x: evt.clientX - rect.left,
+  //       y: evt.clientY - rect.top
+  //     };
+  //   }
+
+  //   isCursorInBoundingBox(bBox, cursorPos) {
+  //     return cursorPos.x > bBox.getX() && cursorPos.x < bBox.getX() + bBox.getW() &&
+  //            cursorPos.y > bBox.getY() && cursorPos.y < bBox.getY() + bBox.getH();
+  //   }
+
+  //   redrawMeasureWithCursorNote(event) {
+  //     // get mouse position
+  //     this.mousePos.current = getMousePos(this.svgElem, event);
+    
+  //     // get selected measure and note
+  //     var vfStaveNote = getSelectedNote();
+  //     var vfStave = getSelectedMeasure();
+    
+  //     // currently support only for replacing rest with a new note
+  //     // building chords feature will be added soon
+  //     if(!vfStaveNote.isRest()) return;
+    
+  //     // get column of selected note on stave
+  //     var bb = vfStave.getBoundingBox();
+  //     var begin = vfStaveNote.getNoteHeadBeginX() - 5;
+  //     bb.setX(begin);
+  //     bb.setW(vfStaveNote.getNoteHeadEndX() - begin + 5);
+  //     // bb.setW(20);
+  //     // bb.draw(this.ctx);
+    
+  //     // mouse cursor is within note column
+  //     if(isCursorInBoundingBox(bb, this.mousePos.current) ) {
+  //       // save mouse position
+  //       this.mousePos.previous = this.mousePos.current;
+  //       // get new note below mouse cursor
+  //       this.selected.cursorNoteKey = getCursorNoteKey();
+    
+  //       this.svgElem.addEventListener('click', this.add.note, false); 
+    
+  //       // redraw only when cursor note changed pitch
+  //       // (mouse changed y position between staff lines/spaces)
+  //       if(this.lastCursorNote !== this.selected.cursorNoteKey) {
+  //         // console.log(this.selected.cursorNoteKey);
+  //         this.draw.selectedMeasure(true);
+    
+  //       }
+  //       // save previous cursor note for latter comparison
+  //       this.lastCursorNote = this.selected.cursorNoteKey;
+  //     }
+  //     // mouse cursor is NOT within note column
+  //     else {
+    
+  //       this.svgElem.removeEventListener('click', this.add.note, false); 
+    
+  //       // mouse cursor just left note column(previous position was inside n.c.)
+  //       if(isCursorInBoundingBox(bb, this.mousePos.previous) ) {
+  //         // redraw measure to erase cursor note
+  //         this.draw.selectedMeasure(false);
+  //         this.mousePos.previous = this.mousePos.current;
+  //         this.lastCursorNote = '';
+  //       }
+  //     }
+    
+  //   }
+  
   
   
 
