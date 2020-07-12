c4f6659adbca0c15d02d0c1a8c9d4ac3cd26fd0b
Aman Gupta <amangupta.1990@ymail.com>
2020-07-12 12:13:22 +0530

    Select bar and stave note

--- src/components/SheetEditor/editor.js/index.js
+++ src/components/SheetEditor/editor.js/index.js
@@ -1,21 +1,16 @@
 import Vex from "vexflow";
 
 const REST_POSITIONS = {
-  "q": "b/4",
-  "h": "b/4",
-  "w": "e/5",
-}
+  q: "b/4",
+  h: "b/4",
+  w: "e/5",
+};
 
-
-
-  
 class Editor {
-
   //eslint-disable-next-line
-  
 
   constructor(svgcontainer) {
-    this.sheet = {}
+    this.sheet = {};
     this.keySig = "C";
     this.timeSigTop = 4;
     this.timeSigBottom = 4;
@@ -28,8 +23,7 @@ class Editor {
     this.staveHeight = 140;
     this.noteWidth = 40;
     this.dotted = "";
-    this.formatter = Vex.Flow.Formatter,
-    this.svgElem = svgcontainer;
+    (this.formatter = Vex.Flow.Formatter), (this.svgElem = svgcontainer);
     this.renderer = new Vex.Flow.Renderer(
       svgcontainer,
       Vex.Flow.Renderer.Backends.SVG
@@ -48,79 +42,113 @@ class Editor {
       },
     };
 
+    // event listerners
+
     // add first stave by default
-    this.addStave()
-    this.addNote(0,0,"c/4","q")
-    this.addNote(0,1,"c/4","q")
-    this.deleteNote(0,0)
+    this.addStave();
+    this.addStave();
+    this.addNote(0, 0, "c/4", "q");
+    this.addNote(0, 1, "c/4", "q");
+    this.deleteNote(0, 0);
     // this.addNote(this.sheet.staves[0],"c/4","q")
     //this.editNote(this.sheet.staves[0],1,"c/4","w")
   }
 
-  
-
-  addStave(index = this.sheet.staves ? this.sheet.staves.length : 0 ) {
-    this.sheet.staves = this.sheet.staves ||  [];
-    let stave = new Vex.Flow.Stave(10,40, this.staveWidth*(this.sheet.staves.length+1));
-    stave.setAttribute("id",index)
+  addStave(index = this.sheet.staves ? this.sheet.staves.length : 0) {
+    this.sheet.staves = this.sheet.staves || [];
+    let stave = new Vex.Flow.Stave(
+      10 + this.staveWidth * index,
+      40,
+      this.staveWidth
+    );
+    stave.setAttribute("id", "vf-" + index);
     stave.addClef(this.clef).addTimeSignature("4/4");
-    stave.notes = new Array(4).fill("q");
 
-    // fill it up with rests 
-    stave.notes =  stave.notes.map((d,noteIndex)=> { 
-      let n =   new Vex.Flow.StaveNote({clef: this.clef, keys: [REST_POSITIONS[d]], duration:d+"r"})
-      n.setAttribute("id",`${index}|${noteIndex}`);
+    //add selectable overlay
+
+    // fill bar with rests
+
+    stave.notes = new Array(4).fill("q");
+    stave.notes = stave.notes.map((d, noteIndex) => {
+      let n = new Vex.Flow.StaveNote({
+        clef: this.clef,
+        keys: [REST_POSITIONS[d]],
+        duration: d + "r",
+      });
+      n.setAttribute("id", `${index}|${noteIndex}`);
 
       return n;
-     } )
-    
-    var voice = new Vex.Flow.Voice({num_beats: 4,  beat_value: 4});
-    voice.addTickables(stave.notes);
-    new Vex.Flow.Formatter().joinVoices([voice]).format([voice], this.staveWidth);
-    stave.voice = voice ;
+    });
 
-    this.sheet.staves.splice(index,0,stave);
-    stave.setContext(this.ctx).draw()
-    voice.draw(this.ctx,stave)
+    this.sheet.staves.splice(index, 0, stave);
+    this.Draw();
+    this.addEventListeners(this.svgElem)
     // draw the notes;
-    
   }
 
-  addNote(staveIndex,noteIndex,noteName,duration){
-    // modify the rest of the stave to join the notes 
-    let stave = this.sheet.staves[staveIndex]
+  addNote(staveIndex, noteIndex, noteName, duration) {
+    // modify the rest of the stave to join the notes
+    let stave = this.sheet.staves[staveIndex];
     let notes = stave.notes;
-    let n  = new Vex.Flow.StaveNote({clef: this.clef, keys: [noteName], duration });
-    n.setAttribute("id",`${staveIndex}|${noteIndex}`);
+    let n = new Vex.Flow.StaveNote({
+      clef: this.clef,
+      keys: [noteName],
+      duration,
+    });
+    n.setAttribute("id", `${staveIndex}|${noteIndex}`);
 
     notes[noteIndex] = n;
-    this.reDraw()
-    
-
+    this.Draw();
   }
-  deleteNote(staveIndex,noteIndex){
-    // convert the note into a rest 
-    let stave = this.sheet.staves[staveIndex]
-    let oldNote =  stave.notes[noteIndex];
-    let n = new Vex.Flow.StaveNote({clef: this.clef, keys: [REST_POSITIONS[oldNote.duration]], duration: oldNote.duration+'r' });
-    n.setAttribute("id",oldNote.id)
-    stave.notes[noteIndex]= n;
-    this.reDraw()
+  deleteNote(staveIndex, noteIndex) {
+    // convert the note into a rest
+    let stave = this.sheet.staves[staveIndex];
+    let oldNote = stave.notes[noteIndex];
+    let n = new Vex.Flow.StaveNote({
+      clef: this.clef,
+      keys: [REST_POSITIONS[oldNote.duration]],
+      duration: oldNote.duration + "r",
+    });
+    n.setAttribute("id", oldNote.id);
+    stave.notes[noteIndex] = n;
+    this.Draw();
   }
 
-
-  reDraw(){
-    this.ctx.clear()
-    this.sheet.staves.map((stave)=>{
+  Draw() {
+    this.ctx.clear();
+    this.sheet.staves.map((stave, staveIndex) => {
       stave.setContext(this.ctx).draw();
-      stave.voice  = new Vex.Flow.Voice({num_beats: 4,  beat_value: 4});
+
+      //add selectable overlay
+      this.ctx.rect(stave.getX(), stave.y, stave.getWidth(), this.staveHeight, {
+        class: "vf-measureRect",
+        id: "vf-" + staveIndex,
+        fill: "transparent",
+      });
+
+      stave.voice = new Vex.Flow.Voice({ num_beats: 4, beat_value: 4 });
       stave.voice.addTickables(stave.notes);
       new Vex.Flow.Formatter().format([stave.voice], this.staveWidth);
-      stave.voice.draw(this.ctx,stave);
-    })
+      stave.voice.draw(this.ctx, stave);
+    });
   }
-  
-  
+
+  addEventListeners(svgElem) {
+    svgElem.addEventListener("click", (event) => {
+      event.preventDefault();
+      let ele = event.target;
+
+      while (
+        ele.classList.value.indexOf("vf-stavenote") < 0 && ele.classList.value.indexOf("vf-measureRect") < 0
+        
+      ){
+        ele = ele.parentElement;
+      }
+        //eslint-disable-next-line
+        console.log(ele);
+    });
+  }
+
   //  getMousePos(canvas, evt) {
   //   var rect = canvas.getBoundingClientRect();
   //     return {
@@ -134,18 +162,18 @@ class Editor {
   //            cursorPos.y > bBox.getY() && cursorPos.y < bBox.getY() + bBox.getH();
   //   }
 
-  //   redrawMeasureWithCursorNote(event) {
+  //   DrawMeasureWithCursorNote(event) {
   //     // get mouse position
   //     this.mousePos.current = getMousePos(this.svgElem, event);
-    
+
   //     // get selected measure and note
   //     var vfStaveNote = getSelectedNote();
   //     var vfStave = getSelectedMeasure();
-    
+
   //     // currently support only for replacing rest with a new note
   //     // building chords feature will be added soon
   //     if(!vfStaveNote.isRest()) return;
-    
+
   //     // get column of selected note on stave
   //     var bb = vfStave.getBoundingBox();
   //     var begin = vfStaveNote.getNoteHeadBeginX() - 5;
@@ -153,46 +181,65 @@ class Editor {
   //     bb.setW(vfStaveNote.getNoteHeadEndX() - begin + 5);
   //     // bb.setW(20);
   //     // bb.draw(this.ctx);
-    
+
   //     // mouse cursor is within note column
   //     if(isCursorInBoundingBox(bb, this.mousePos.current) ) {
   //       // save mouse position
   //       this.mousePos.previous = this.mousePos.current;
   //       // get new note below mouse cursor
   //       this.selected.cursorNoteKey = getCursorNoteKey();
-    
-  //       this.svgElem.addEventListener('click', this.add.note, false); 
-    
-  //       // redraw only when cursor note changed pitch
+
+  //       this.svgElem.addEventListener('click', this.add.note, false);
+
+  //       // Draw only when cursor note changed pitch
   //       // (mouse changed y position between staff lines/spaces)
   //       if(this.lastCursorNote !== this.selected.cursorNoteKey) {
   //         // console.log(this.selected.cursorNoteKey);
   //         this.draw.selectedMeasure(true);
-    
+
   //       }
   //       // save previous cursor note for latter comparison
   //       this.lastCursorNote = this.selected.cursorNoteKey;
   //     }
   //     // mouse cursor is NOT within note column
   //     else {
-    
-  //       this.svgElem.removeEventListener('click', this.add.note, false); 
-    
+
+  //       this.svgElem.removeEventListener('click', this.add.note, false);
+
   //       // mouse cursor just left note column(previous position was inside n.c.)
   //       if(isCursorInBoundingBox(bb, this.mousePos.previous) ) {
-  //         // redraw measure to erase cursor note
+  //         // Draw measure to erase cursor note
   //         this.draw.selectedMeasure(false);
   //         this.mousePos.previous = this.mousePos.current;
   //         this.lastCursorNote = '';
   //       }
   //     }
-    
+
   //   }
-  
-  
-  
 
+  //  getCursorNoteKey() {
+  //   // find the mouse position and return the correct note for that position.
+  //   var y = gl_VfStaves[this.selected.measure.id.split('m')[1]].y;
+  //   // var y = this.selected.measure.y;
+  //   var notesArray = ['c/','d/','e/','f/','g/','a/','b/'];
+  //   var count = 0;
 
+  //   for(var i = 5; i >= 0; i--){
+  //     for(var l = 0; l < notesArray.length; l++){
+  //       var noteOffset = (count * 35) - (l * 5 - 17);
+  //       if(this.mousePos.current.y >= y + noteOffset && this.mousePos.current.y <= 5 + y + noteOffset){
+  //         var cursorNoteKey = notesArray[l] + (i+1);
+  //         var found = true;
+  //         break;
+  //       }
+  //       if(found == true){
+  //         break;
+  //       }
+  //     }
+  //     count++;
+  //   }
+  //   return cursorNoteKey;
+  // }
 }
 
 export default Editor;
